7978d8cec80fbc6f3fb33fc9322b343807f7b8dd
Falco <falco@falco.bz>
2017-07-16 16:59:11 +0200

    transport file - fix

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ touch /var/log/messages
 chmod o+r /var/log/*
 
 postmap /etc/postfix/user/recipient_access
-postmap /etc/postfix/user/relay_domains
+postmap /etc/postfix/user/transport
 postmap /etc/postfix/user/sender_access
 
 /usr/bin/supervisord
