cc523c421179e831dcb3b68c0e77775275432ad0
Falco <falco@falco.bz>
2017-07-29 02:40:40 +0200

    improve postmap in /etc/postfix/user

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ touch /var/log/maillog
 touch /var/log/messages
 chmod o+r /var/log/*
 
-postmap /etc/postfix/user/recipient_access
-postmap /etc/postfix/user/transport
-postmap /etc/postfix/user/sender_access
+pushd /etc/postfix/user/
+for f in `find -type f`; do
+    c=`awk '/^#run:/ {gsub (/^#run:/,""); print $0}' $f`
+    [ -z "$c" ] || ( echo + $c $f && eval $c $f )
+done
+popd
 
 /usr/bin/supervisord
